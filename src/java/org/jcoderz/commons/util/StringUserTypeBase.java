e81ff5d3bd8fc5ea89b9625996fbe1843e5828d0
tbodemer <no_author@no_author>
2008-04-10 13:52:14 +0000

    Hibernate User Types for StrongTypes

---
+++ src/java/org/jcoderz/commons/util/StringUserTypeBase.java
@@ -0,0 +1,120 @@
+/*
+ * $Id: ArraysUtil.java 107 2006-12-06 11:27:11Z amandel $
+ *
+ * Copyright 2006, The jCoderZ.org Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *    * Neither the name of the jCoderZ.org Project nor the names of
+ *      its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jcoderz.commons.util;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.hibernate.HibernateException;
+
+/**
+ *
+ * This class is used by the Hibernate binding for RestrictedStrings.
+ * A subclass has to implement the <code>fromString</code> method
+ * for the particular RestrictedString. Be aware that VARCHAR is
+ * restricted to 1000 Unicode characters!
+ *
+ * @author thomas.bodemer
+ */
+public abstract class StringUserTypeBase
+    extends UserTypeBase
+{
+    private static final int[] SQL_TYPES = {Types.VARCHAR};
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object nullSafeGet (ResultSet resultSet, String[] types,
+        Object owner)
+        throws HibernateException, SQLException
+    {
+        final String value = resultSet.getString(types[0]);
+        final Object result;
+        if (resultSet.wasNull())
+        {
+            result = null;
+        }
+        else if (value.length() == 0)
+        {
+            result = getEmptyOrNull();
+        }
+        else
+        {
+            result = fromString(value);
+        }
+        return result;
+    }
+
+    /**
+     * Implement this method for the particular RestrictedString.
+     *
+     * @return either null or <code>fromString("")</code> if the
+     *   StrongType permits this.
+     */
+    public abstract Object getEmptyOrNull ();
+
+    /**
+     * Implement this method for the particular RestrictedString.
+     *
+     * @param value the String representation of the mapped class
+     * @return the instance of the mapped class
+     */
+    public abstract Object fromString (String value);
+
+    /**
+     * {@inheritDoc}
+     */
+    public void nullSafeSet (PreparedStatement statement, Object value,
+        int index)
+        throws HibernateException, SQLException
+    {
+        if (value != null)
+        {
+            statement.setString(index, value.toString());
+        }
+        else
+        {
+            statement.setNull(index, Types.VARCHAR);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int[] sqlTypes ()
+    {
+      return (int[]) SQL_TYPES.clone();
+    }
+}
---
+++ src/java/org/jcoderz/commons/util/UserTypeBase.java
@@ -0,0 +1,136 @@
+/*
+ * $Id: ArraysUtil.java 107 2006-12-06 11:27:11Z amandel $
+ *
+ * Copyright 2006, The jCoderZ.org Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *    * Neither the name of the jCoderZ.org Project nor the names of
+ *      its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jcoderz.commons.util;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.hibernate.HibernateException;
+import org.hibernate.usertype.UserType;
+
+/**
+ *
+ * Base class for Hibernate User Types. This class is used by the
+ * Hibernate binding for StrongTypes. There exist subclasses for
+ * the specific kinds of StrongTypes like RestrictedString.
+ *
+ * @author thomas.bodemer
+ */
+public abstract class UserTypeBase implements UserType
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract int[] sqlTypes ();
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract Class returnedClass ();
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean equals (Object x, Object y)
+    {
+      return ObjectUtil.equals(x, y);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object deepCopy (Object value)
+    {
+      return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isMutable ()
+    {
+      return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract Object nullSafeGet (ResultSet resultSet, String[] types,
+        Object owner)
+      throws HibernateException, SQLException;
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract void nullSafeSet (PreparedStatement statement, Object value,
+        int index)
+      throws HibernateException, SQLException;
+
+    /**
+     * {@inheritDoc}
+     */
+    public int hashCode (Object x) throws HibernateException
+    {
+      return x.hashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Serializable disassemble (Object value)
+        throws HibernateException
+    {
+      return (Serializable) value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object assemble (Serializable cached, Object owner)
+        throws HibernateException
+    {
+      return cached;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object replace (Object original, Object target, Object owner)
+        throws HibernateException
+    {
+      return original;
+    }
+}

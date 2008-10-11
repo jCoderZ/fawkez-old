bf932d489b4c002b0ef8517f211181c8420f8f0c
amandel <andreas.mandel@gmail.com>
2008-10-11 08:44:00 +0000

    Don't pass an empty string as cvsSuffix argument to java2html.

--- src/java/org/jcoderz/phoenix/report/JcReportAntTask.java
+++ src/java/org/jcoderz/phoenix/report/JcReportAntTask.java
@@ -668,7 +668,7 @@ public class JcReportAntTask
       cmd.createArgument().setValue("-cvsBase");
       cmd.createArgument().setValue(mWebRcsBase);
 
-      if (mWebRcsSuffix != null)
+      if (StringUtil.isNullOrBlank(mWebRcsSuffix))
       {
           cmd.createArgument().setValue("-cvsSuffix");
           cmd.createArgument().setValue(mWebRcsSuffix);
